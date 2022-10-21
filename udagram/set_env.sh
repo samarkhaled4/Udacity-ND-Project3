9d63ca0b7db6ea80212c88fea5d71024ad6d1c0f
samarkhaled4 <samarkhaled.iti@gmail.com>
2022-10-21 03:48:05 +0200

    initial commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.cdmsjtsxkkkv.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=hostingudagram
+export AWS_REGION=us-east-1
+export AWS_PROFILE=https://727773902434.signin.aws.amazon.com/console
+export JWT_SECRET="hhhhhhhhhhhhhhhh"
+export URL=http://hostingudagram.s3-website-us-east-1.amazonaws.com
